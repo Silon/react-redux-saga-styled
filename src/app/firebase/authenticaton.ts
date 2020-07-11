081499ccf2718a16c9c0336ab695ca5df6a18ecf
Damian <nowakowski.damian@gmail.com>
2020-07-11 14:43:34 +0200

    🎨 Code styling

--- src/app/firebase/authenticaton.ts
+++ src/app/firebase/authenticaton.ts
@@ -9,5 +9,3 @@ export const loginUser = async ({ email, password }: TLoginUserData) =>
   await firebaseAuth.signInWithEmailAndPassword(email, password)
 
 export const logoutUser = async () => await firebaseAuth.signOut()
-
-// onAuthStateChanged
--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,32 +1,25 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { routerMiddleware } from 'connected-react-router'
 import { createBrowserHistory } from 'history'
-import sagas from "./sagas";
-import createRootReducer from "./reducers";
+import sagas from './sagas'
+import createRootReducer from './reducers'
 
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState = {}) {
-
   const sagaMiddleware = createSagaMiddleware()
 
   const composeEnhancers = composeWithDevTools({
     // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-  });
+  })
 
   const store = createStore(
     createRootReducer(history), // root reducer with router state
     preloadedState,
-    composeEnhancers(
-      applyMiddleware(
-        routerMiddleware(history),
-        sagaMiddleware,
-        thunkMiddleware,
-      )
-    )
+    composeEnhancers(applyMiddleware(routerMiddleware(history), sagaMiddleware, thunkMiddleware))
   )
   sagaMiddleware.run(sagas)
 
